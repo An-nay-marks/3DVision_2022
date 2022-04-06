d60e789b0e33e9956497da855a9cdbba165f4323
Lucas Weitzendorf <lweitzendorf@student.ethz.ch>
2022-04-06 22:25:06 +0200

    Refactored argument parsing into separate function

--- main.py
+++ main.py
@@ -1,54 +1,69 @@
 import argparse
-import sys
+
 from data_handling.detect_faces import face_detection
-from data_handling.online_pipeline import run_online_pipeline
 from data_handling.offline_pipeline import run_offline_pipeline
+from data_handling.online_pipeline import run_online_pipeline
 from utils_3DV import DETECTORS, CLASSIFIERS
+
+
+def parse_args():
+    functions_dic = {'detect_faces': face_detection,
+                     'online_pipeline': run_online_pipeline,
+                     'offline_pipeline': run_offline_pipeline}
+
+    # parent parser
+    parser = argparse.ArgumentParser(description="Implementation of the 3D Face Reconstruction Pipeline using Videos",
+                                     add_help=False)
+    default_function = list(functions_dic.keys())[0]
+    parser.add_argument('-f', '--function', default=default_function, type=str,
+                        help=f"Give the function you would like to run. Default = \"{default_function}\".\n"
+                             "Currently available:\n".join(functions_dic.keys()))
+    function_name = parser.parse_known_args()[0].function.lower()
+
+    # specific parser to each function
+    specific_parser = argparse.ArgumentParser(description="Parser for specific arguments for called function")
+    specific_parser.add_argument('-v', '--video_path', type=str,
+                                 help="Video Path where the video to be analyzed can be found at, "
+                                      "starting at project root folder")
+    specific_parser.add_argument('-d', '--detector', type=str,
+                                 help="Detector Model, default is scrfd. "
+                                      "Currently available Detectors: ".format(DETECTORS),
+                                 default="scrfd")
+
+    if function_name == 'detect_faces':
+        specific_parser.add_argument('-t', '--target_path', type=str,
+                                     help="Target Path, where the detected images are saved in. "
+                                          "Defaults to None, if they shouldn't be saved",
+                                     default=None)
+        specific_parser.add_argument('-r', '--required_size', type=int,
+                                     help="Patch size output (Tuple(int,int)), if specific size is required. "
+                                          "Defaults to None, if patch size is neglectable",
+                                     default=None)
+    elif function_name == 'online_pipeline':
+        specific_parser.add_argument('-c', '--classifier', type=str,
+                                     help="Classification Model, default is one self-implemented classifier, "
+                                          "that first embeds the patch and then measures the similarity. "
+                                          "Currently available Classifiers (except meanshift): ".format(CLASSIFIERS),
+                                     default="online")
+    elif function_name == 'offline_pipeline':
+        specific_parser.add_argument('-c', '--classifier', type=str,
+                                     help="Classification Model, default is meanshift. "
+                                          "Currently available Classifiers: ".format(CLASSIFIERS),
+                                     default="meanshift")
+    else:
+        msg = "Error: Make sure you spelled the function type correctly. " \
+              "Currently available functions: " + ", ".join(functions_dic.keys())
+        print(msg)
+        return None, None
+
+    function = functions_dic[function_name]
+    specific_args = specific_parser.parse_known_args()[0]
+    return function, specific_args
+
 
 if __name__ == '__main__':
     """The main function to call all functions in this Repo with
     """
-    
-    # dict of functions that can be called
-    functions_dic = {'detect_faces': face_detection,
-                     'online_pipeline': run_online_pipeline,
-                     'offline_pipeline': run_offline_pipeline}
-    
-    # parent parser
-    parser = argparse.ArgumentParser(description="Implementation of the 3D Face Reconstruction Pipeline using Videos", add_help=False)
-    parser.add_argument('-f', '--function', default="detect_faces", type=str, help="Give the function you would like to run. Default = \"detect_faces\".\nCurrently available:\n".join(functions_dic.keys()))
-
-    # specific parser to each function
-    specific_parser = argparse.ArgumentParser(description = "Parser for specific arguments for called function")
-    args = parser.parse_known_args()[0]
-    if args.function.lower() == 'detect_faces':
-        specific_parser.add_argument('-v', '--video_path', type=str, help="Video Path where the video to be analyzed can be found at, starting at project root folder")
-        specific_parser.add_argument('-t', '--target_path', type=str, help="Target Path, where the detected images are saved in. Defaults to None, if they shouldn't be saved", default=None)
-        specific_parser.add_argument('-d', '--detector', type=str, help="Detector Model, default is scrfd. Currently available Detectors: ".format(DETECTORS), default="scrfd")
-        specific_parser.add_argument('-r', '--required_size', type=int, help="Patch size output (Tuple(int,int)), if specific size is required. Defaults to None, if patch size is neglectable", default=None)
-        filter_args = ["video_path", "target_path", "detector", "required_size"]
-    elif args.function.lower() == 'online_pipeline':
-        specific_parser.add_argument('-v', '--video_path', type=str, help="Video Path where the video to be analyzed can be found at, starting at project root folder")
-        specific_parser.add_argument('-d', '--detector', type=str, help="Detector Model, default is scrfd. Currently available Detectors: ".format(DETECTORS), default="scrfd")
-        specific_parser.add_argument('-c', '--classifier', type=str, help="Classification Model, default is one self-implemented classifier, that first embeds the patch and then measures the similarity. Currently available Classifiers (except meanshift): ".format(CLASSIFIERS), default="online")
-        filter_args = ["video_path", "detector", "classifier"]
-    elif args.function.lower() == 'offline_pipeline':
-        specific_parser.add_argument('-v', '--video_path', type=str, help="Video Path where the video to be analyzed can be found at, starting at project root folder")
-        specific_parser.add_argument('-d', '--detector', type=str, help="Detector Model, default is scrfd. Currently available Detectors: ".format(DETECTORS), default="scrfd")
-        specific_parser.add_argument('-c', '--classifier', type=str, help="Classification Model, default is meanshift. Currently available Classifiers: ".format(CLASSIFIERS), default="meanshift")
-        filter_args = ["video_path", "detector", "classifier"]
-    else:
-        msg = "Error: Make sure you spelled the function type correctly. Currently available functions: " + ", ".join(functions_dic.keys())
-        print(msg)        
-        sys.exit(0)
-    
-    # call function with function-specific arguments
-    # try:     
-    spec_args = specific_parser.parse_known_args()[0]
-    fun = functions_dic[args.function.lower()]
-    fun(**{k: v for k, v in spec_args._get_kwargs()})
-    """except (KeyError, AttributeError) as err:
-        print(err)
-        print("Error: Make sure the parameters for "+ args.function + " are correct. They have to be contained in this list: " + ", ".join(filter_args))"""
-    #except ModuleNotFoundError as mr:
-    #    print(f"If the Error Message is \"No module named 'keras.engine.topology'\", please check out https://stackoverflow.com/questions/68862735/keras-vggface-no-module-named-keras-engine-topology.\nError message: {mr}")
+    fun, args = parse_args()
+    if fun is not None:
+        fun(**vars(args))
--- recognition/vgg.py
+++ recognition/vgg.py
@@ -1,24 +1,25 @@
-# code adapted from https://machinelearningmastery.com/how-to-perform-face-recognition-with-vggface2-convolutional-neural-network-in-keras/
-from keras_vggface.vggface import VGGFace
+# code adapted from https://machinelearningmastery.com/how-to-perform-face-recognition-with-
+# vggface2-convolutional-neural-network-in-keras/
 import cv2
 from keras_vggface.vggface import VGGFace
 from keras_vggface.utils import preprocess_input
 from keras_vggface.utils import decode_predictions
 from numpy import expand_dims
 
-class VGGEncoderAndClassifier():
+
+class VGGEncoderAndClassifier:
     def __init__(self, threshold=0.3):
-        self.classifier = VGGFace(model ='resnet50')
+        self.classifier = VGGFace(model='resnet50')
         self.threshold = threshold
     
     def classify(self, img):
         img = img.astype('float32')
-        img = cv2.resize(img, (224,224))
+        img = cv2.resize(img, (224, 224))
         img = expand_dims(img, axis=0)
         sample = preprocess_input(img, version=2)
         class_code = decode_predictions(self.classifier.predict(sample))
         if class_code[0][0][1] < self.threshold:
             print(class_code[0])
-            return -1,-1
+            return -1, -1
         # highest scored name and probability
         return class_code[0][0][0].split(' ')[1], class_code[0][0][1]
