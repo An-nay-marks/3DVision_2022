Fixed norm call in cosine similarity calculation

recognition/arcface.py
@@ -24,7 +24,7 @@ class ArcFace:
 
     @staticmethod
     def similarity(enc1, enc2):
-        return enc1 @ enc2 / (np.norm(enc1) * np.norm(enc2))
+        return enc1 @ enc2 / (np.linalg.norm(enc1) * np.linalg.norm(enc2))
 
     @staticmethod
     def distance(enc1, enc2):

reconstruction/deca.py
@@ -27,6 +27,6 @@ class DECAFaceReconstruction(DECA):
         img = torch.from_numpy(img).unsqueeze(0).float().to(self.device)
         img.div_(255)
 
-        code_dict = super().encode(img)
-        op_dict, _ = super().decode(code_dict)
+        code_dict = self.encode(img)
+        op_dict, _ = self.decode(code_dict)
         return op_dict