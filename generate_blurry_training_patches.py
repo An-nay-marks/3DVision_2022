1e3dc102d8949afdc8f7fbb2a04d55979d971f65
An-nay-marks <87384853+An-nay-marks@users.noreply.github.com>
2022-06-11 15:43:41 +0200

    now transformation, swirling, change in brightness

--- generate_blurry_training_patches.py
+++ generate_blurry_training_patches.py
@@ -1,13 +1,4 @@
 from reconstruction.improving_deca.dataset import augment_patches
 
 if __name__=="__main__":
-    augment_patches(downscale = 0.01, start_counter_offset = 0) # include some very bad data, because that is often the case for single frames from videos
-    augment_patches(downscale = 0.01, start_counter_offset = 1, flip=True)
-    augment_patches(downscale = 0.05, start_counter_offset = 2)
-    augment_patches(downscale = 0.05, start_counter_offset = 3, flip=True)
-    augment_patches(downscale = 0.2, start_counter_offset = 4)
-    augment_patches(downscale = 0.2, start_counter_offset = 5, flip=True)
-    augment_patches(downscale = 0.5, start_counter_offset = 6)
-    augment_patches(downscale = 0.5, start_counter_offset = 7, flip=True)
-    augment_patches(downscale = 0.8, start_counter_offset = 8)
-    augment_patches(downscale = 0.8, start_counter_offset = 9, flip=True)
+    augment_patches(start_counter_offset=0)
--- reconstruction/improving_deca/dataset.py
+++ reconstruction/improving_deca/dataset.py
@@ -1,3 +1,4 @@
+import itertools
 import os
 import torch
 import numpy as np
@@ -8,20 +9,41 @@ from pathlib import Path
 
 from skimage.io import imread, imsave
 from skimage.util import img_as_ubyte
-from skimage.transform import estimate_transform, warp, rescale, resize
+from skimage.transform import estimate_transform, warp, rescale, resize, swirl, PiecewiseAffineTransform
+from skimage import exposure
 from torch.utils.data import Dataset
 from torch.utils.data import dataset
 from utils_3DV import ROOT_DIR, check_dataset, pad_with_zeros
 
 
-def augment_patches(downscale, start_counter_offset, flip=False):
-    """Generates downscaled images from the NoW Dataset and can additionally flip it.
+def augment_patches(start_counter_offset):
+    """Downscaling, flipping, transformation, swirling, change in brightness and contrast
 
     Args:
-        downscale (float): between 0 and 1 to downscale, otherwise image will be upscaled
-        start_counter_offset (int) offset for the FaMoS Number in order to avoid collision with existing data (always starting at 200000 + global offset)
-        flip (bool): whether to flip the patch or not
+        start_counter_offset (int) offset for the FaMoS Number in order to avoid collision with existing data, increment with each call of this function by 1
     """
+    def aff_trans(image):
+        # adapted from scikit docs
+        rows, cols = image.shape[0], image.shape[1]
+        src_cols = np.linspace(0, cols, 20)
+        src_rows = np.linspace(0, rows, 10)
+        src_rows, src_cols = np.meshgrid(src_rows, src_cols)
+        src = np.dstack([src_cols.flat, src_rows.flat])[0]
+        # add sinusoidal oscillation to row coordinates
+        dst_rows = src[:, 1] - np.sin(np.linspace(0, 3 * np.pi, src.shape[0])) * 7
+        dst_cols = src[:, 0]
+        dst_rows *= 1.5
+        dst_rows -= 1.5 * 7
+        dst = np.vstack([dst_cols, dst_rows]).T
+        aff = PiecewiseAffineTransform()
+        aff.estimate(src, dst)
+        return warp(image, aff)
+    def exp_trans(image, g, l):
+        # gamma bigger 1: darker, smaller 1: lighter
+        # log (gain) bigger: smaller contrast, gain smaller: bigger contrast: O = gain*log(1 + I) 
+        gamma_corrected = exposure.adjust_gamma(image, g)
+        log_corrected = exposure.adjust_gamma(gamma_corrected, l)
+        return log_corrected
     check_dataset()
     data_set = NoWDataset()
     iphone_pictures_folder = data_set.imagefolder
@@ -31,58 +53,68 @@ def augment_patches(downscale, start_counter_offset, flip=False):
     base_number = str(200000 + start_counter_offset)
     counter = 1
     counter_len = 5
-    img_counter = 1303 * (start_counter_offset+1) # there will be max 1302 images created per run
-    img_counter_len = 4
-    print(f"Starting to blurr images by factor {downscale}...")
+    img_counter = 1303 + (1303 * 200 * (start_counter_offset)) # there will be approx 1302 * 200 images created per run
+    img_counter_len = 6
+    downscales = [0.01, 0.05, 0.1, 0.2, 0.5]
+    flips = [lambda x:x, lambda x:x[:, ::-1, :]] # x, -y, color, horizontal flip
+    swirls = [lambda x:x, lambda x: swirl(x, strength = 2, radius = 100, rotation=0.2)] # not too much of a swirl
+    affine_trans = [lambda x:x, lambda x: aff_trans(x)]
+    exposures = [lambda x:x, lambda x: exp_trans(x, 0.8, 0.8), lambda x: exp_trans(x, 0.8, 1.5), lambda x: exp_trans(x, 1.5, 0.8), lambda x: exp_trans(x, 2, 1.5), lambda x: exp_trans(x, 1.3, 1.5)]
+    
+    combined_augmentation = list(itertools.product(downscales, flips, swirls, affine_trans, exposures))
     
     previous_FaMoS = ""
     for idx in tqdm(range(len(data_set))):
-        dict = data_set[idx]
-        # blurr iphone_pictures
-        img = dict['original_image']
-        img_downscaled = rescale(img, scale = (downscale, downscale, 1)) # x, y, rgb (don't change rgb)
-        if flip:
-            img_downscaled = img_downscaled[:, ::-1, :] # x, -y, color, horizontal flip
-        target_image = img_as_ubyte(resize(img_downscaled, output_shape = img.shape)) # keep uint8 instead of float after resize
-        # extract path names and target FaMoS nunmber
-        FaMoS_number, expression_type, image_name = dict['imagename'].split('/') # e.g. FaMoS_180618_03331_TA/multiview_neutral/IMG_1216
-        target_FaMoS_number = f"FaMoS_{base_number}_{pad_with_zeros(counter, counter_len)}_TA/"
-        
-        # copy scans
-        scan_folder = os.path.join(scans_path, FaMoS_number)
-        scan_name = os.listdir(scan_folder)[0] # with file extension already
-        scan_path = os.path.join(scan_folder, scan_name)
-        target_scan_folder = os.path.join(scans_path, target_FaMoS_number)
-        target_scan_path = os.path.join(target_scan_folder, scan_name) # use same scan file name, different folder
-        if not os.path.exists(target_scan_folder): # one scan for multiple images
-            Path(target_scan_folder).mkdir(parents = True)
-            shutil.copyfile(src = scan_path, dst = target_scan_path)
-        
-        # copy detected face
-        target_image_name = f"IMG_{pad_with_zeros(img_counter, img_counter_len)}"
-        detected_face_path = os.path.join(detected_face_folder, FaMoS_number, expression_type, f"{image_name}.npy")
-        target_detected_face_folder = os.path.join(detected_face_folder, target_FaMoS_number, expression_type)
-        target_detected_face_path = os.path.join(target_detected_face_folder, f"{target_image_name}.npy")
-        Path(target_detected_face_folder).mkdir(parents = True, exist_ok = True)
-        shutil.copyfile(src = detected_face_path, dst = target_detected_face_path)
-        
-        # save blurred images
-        target_image_folder = os.path.join(iphone_pictures_folder, target_FaMoS_number, expression_type)
-        target_image_path = os.path.join(target_image_folder, f"{target_image_name}.jpg")
-        Path(target_image_folder).mkdir(parents = True, exist_ok = True)
-        imsave(target_image_path, target_image)
-        
-        # increase counters
-        if not previous_FaMoS == FaMoS_number: # multiple images might have the same FaMoS number
-            if not previous_FaMoS == "": # don't increase in first loop iteration
-                counter += 1
-            previous_FaMoS = FaMoS_number
-        img_counter += 1
-        
-        # save data path to "imagespathsvalidation.txt"
-        target_line = os.path.join(target_FaMoS_number, f"{expression_type}/{target_image_name}.jpg")
-        with open(data_path_txt, 'a') as f:
-            f.write(f"{target_line}\n")
+        for downscale, flip, swir, aff, exp in combined_augmentation:
+            dict = data_set[idx]
+            # augment iphone_pictures
+            img = dict['original_image']
+            img_downscaled = rescale(img, scale = (downscale, downscale, 1)) # x, y, rgb (don't change rgb)
+            flipped = flip(img_downscaled)
+            swirled = swir(flipped)
+            transformed = aff(swirled)
+            corrected = exp(transformed)
+            target_image = img_as_ubyte(resize(corrected, output_shape = img.shape)) # keep uint8 instead of float
+            
+            # extract path names and target FaMoS nunmber
+            FaMoS_number, expression_type, image_name = dict['imagename'].split('/') # e.g. FaMoS_180618_03331_TA/multiview_neutral/IMG_1216
+            target_FaMoS_number = f"FaMoS_{base_number}_{pad_with_zeros(counter, counter_len)}_TA/"
+            
+            # copy scans
+            scan_folder = os.path.join(scans_path, FaMoS_number)
+            scan_name = os.listdir(scan_folder)[0] # with file extension already
+            scan_path = os.path.join(scan_folder, scan_name)
+            target_scan_folder = os.path.join(scans_path, target_FaMoS_number)
+            target_scan_path = os.path.join(target_scan_folder, scan_name) # use same scan file name, different folder
+            if not os.path.exists(target_scan_folder): # one scan for multiple images
+                Path(target_scan_folder).mkdir(parents = True)
+                shutil.copyfile(src = scan_path, dst = target_scan_path)
+            
+            # copy detected face
+            target_image_name = f"IMG_{pad_with_zeros(img_counter, img_counter_len)}"
+            detected_face_path = os.path.join(detected_face_folder, FaMoS_number, expression_type, f"{image_name}.npy")
+            target_detected_face_folder = os.path.join(detected_face_folder, target_FaMoS_number, expression_type)
+            target_detected_face_path = os.path.join(target_detected_face_folder, f"{target_image_name}.npy")
+            Path(target_detected_face_folder).mkdir(parents = True, exist_ok = True)
+            shutil.copyfile(src = detected_face_path, dst = target_detected_face_path)
+            
+            # save blurred images
+            target_image_folder = os.path.join(iphone_pictures_folder, target_FaMoS_number, expression_type)
+            target_image_path = os.path.join(target_image_folder, f"{target_image_name}.jpg")
+            Path(target_image_folder).mkdir(parents = True, exist_ok = True)
+            imsave(target_image_path, target_image)
+            
+            # increase counters
+            if not previous_FaMoS == FaMoS_number: # multiple images might have the same FaMoS number
+                if not previous_FaMoS == "": # don't increase in first loop iteration
+                    counter += 1
+                previous_FaMoS = FaMoS_number
+            img_counter += 1
+            
+            # save data path to "imagespathsvalidation.txt"
+            target_line = os.path.join(target_FaMoS_number, f"{expression_type}/{target_image_name}.jpg")
+            with open(data_path_txt, 'a') as f:
+                f.write(f"{target_line}\n")
     print("...Done!")
 
 # class WeightingPatchesDataset(dataset.Dataset):
